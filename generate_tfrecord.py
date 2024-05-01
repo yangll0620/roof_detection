a60d3c53083d48212156fab930391b7de51bc6ba
Lingling Yang <yll@macbook-pro-5.mynetworksettings.com>
2024-05-01 11:12:32 -0400

    before Docker

--- generate_tfrecord.py
+++ generate_tfrecord.py
@@ -1,9 +1,13 @@
 import os
-import io
+import numpy as np
 import sys
 import glob
 import xml.etree.ElementTree as ET
+from PIL import Image
+
 import tensorflow as tf
+
+
 from object_detection.utils import dataset_util
 
 
@@ -13,15 +17,29 @@ class_labels = {
 }
 
 
-def encode_image(image_file):
-    with tf.io.gfile.GFile(image_file, 'rb') as f:
-        return f.read()
 
-
-
-# create TF example
-def create_tf_example(xml_file, image_file, class_name2labels):
+def read_image_to_numpy(image_file):
+    """ Read image from file and convert to NumPy array
+            convert to RGB format if RGBA
     """
+    image = Image.open(image_file)
+    if image.mode == 'RGBA':
+        image = image.convert('RGB')
+    
+    return np.array(image)
+
+
+def encode_image(image):
+    """Encode image as byte string 
+    """
+    return tf.io.encode_jpeg(image).numpy()
+
+
+
+# 
+def create_tf_example(xml_file, image_file, class_name2labels):
+    """ create an TF example using xml_file and image_file
+
         Args:
             class_labels: a dictionary with key is the class name and the value is the class label, 
                 i.e class_labels = {'helmet': 1, 'head': 2, 'person': 3}
@@ -41,7 +59,8 @@ def create_tf_example(xml_file, image_file, class_name2labels):
     image_height = int(xml.find('size/height').text)
 
     # read image
-    encoded_image_data = encode_image(image_file)
+    image_np = read_image_to_numpy(image_file)
+    encoded_image_data = encode_image(image_np)
 
     # Extract bounding box coordinates and class labels
     xmins, xmaxs, ymins, ymaxs, classes_text, classes = [], [], [], [], [], []
@@ -56,20 +75,21 @@ def create_tf_example(xml_file, image_file, class_name2labels):
         ymaxs.append(float(bbox.find('ymax').text) / image_height)
 
     # Create TF example
-    tf_example = tf.train.Example(features=tf.train.Features(feature={
+    tf_features = tf.train.Features(feature={
+        'image/data': dataset_util.bytes_feature(encoded_image_data),
         'image/height': dataset_util.int64_feature(image_height),
         'image/width': dataset_util.int64_feature(image_width),
         'image/filename': dataset_util.bytes_feature(image_filename.encode('utf8')),
-        'image/source_id': dataset_util.bytes_feature(image_filename.encode('utf8')),
-        'image/image_data': dataset_util.bytes_feature(encoded_image_data),
         'image/format': dataset_util.bytes_feature(image_format.encode('utf8')),
+        
         'image/object/bbox/xmin': dataset_util.float_list_feature(xmins),
         'image/object/bbox/xmax': dataset_util.float_list_feature(xmaxs),
         'image/object/bbox/ymin': dataset_util.float_list_feature(ymins),
         'image/object/bbox/ymax': dataset_util.float_list_feature(ymaxs),
         'image/object/class/text': dataset_util.bytes_list_feature(classes_text),
         'image/object/class/label': dataset_util.int64_list_feature(classes),
-    }))
+    })
+    tf_example = tf.train.Example(features=tf_features)
 
     return tf_example
 
